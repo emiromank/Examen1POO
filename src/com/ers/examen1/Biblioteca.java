fff45e25e5798a32f1afccece254804e96ecb752
Roman <Roman@172.22.1.2>
2022-02-10 17:06:01 -0600

    Clase Biblioteca creada

--- src/com/ers/examen1/Biblioteca.java
+++ src/com/ers/examen1/Biblioteca.java
@@ -1,5 +1,18 @@
 package com.ers.examen1;
 
 public class Biblioteca {
-
+	private String nombre;
+	private String direccion;
+	private Estudiantes[] estudiantes  = new Estudiantes[5];
+	private Profesores[] profesores = new Profesores[5];
+	private int numEs;
+	private int numProf;
+	
+	Biblioteca (String nombre, String direccion){
+		this.nombre = nombre;
+		this.direccion = direccion;
+	}
+	
+	
+	
 }
