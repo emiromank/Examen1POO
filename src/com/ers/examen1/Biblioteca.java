b346fb8b4e90b38a2d421cc3dbd0be0e57121501
Roman <Roman@172.22.1.2>
2022-02-10 17:43:25 -0600

    Main creado, nuevos metodos en biblioteca

--- src/com/ers/examen1/Biblioteca.java
+++ src/com/ers/examen1/Biblioteca.java
@@ -3,10 +3,10 @@ package com.ers.examen1;
 public class Biblioteca {
 	private String nombre;
 	private String direccion;
-	private Estudiantes[] estudiantes  = new Estudiantes[5];
-	private Profesores[] profesores = new Profesores[5];
-	private int numEs;
-	private int numProf;
+	public Estudiantes[] estudiantes  = new Estudiantes[5];
+	public Profesores[] profesores = new Profesores[5];
+	private int numEs=0;
+	private int numProf=0;
 	
 	Biblioteca (String nombre, String direccion){
 		this.nombre = nombre;
@@ -14,5 +14,19 @@ public class Biblioteca {
 	}
 	
 	
+	public void agregarEstudiante(String id, String nombre, String apellido,String carrera, String plan, String correo, double semestre) {
+		Estudiantes e = new Estudiantes(id,nombre,apellido,carrera,plan,correo,semestre);
+		this.estudiantes[numEs]=e;
+		this.numEs++;
+	}
+
+	public void agregarProfesor(String id, String nombre, String apellido,String materiaImpartida, String tipoProfesor, double extension, double horasPorSemana) {
+		Profesores e = new Profesores(id,nombre,apellido,materiaImpartida,tipoProfesor,extension,horasPorSemana);
+		this.profesores[numProf]=e;
+		this.numProf++;
+	}
+	
+	
 	
 }
+
---
+++ src/com/ers/examen1/Main.java
@@ -0,0 +1,15 @@
+package com.ers.examen1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Biblioteca bib = new Biblioteca("Libreria El Pendulo","Naranjo 280, Sta. Maria, CDMX") ;
+		
+		bib.agregarEstudiante("a218", "Pepe", "Pecas", "Comunicacion", "SUJ", "pp21@gmail.com", 5);
+		bib.agregarProfesor("p211", "Jaime", "Maussan", "Vida Extraterrestre I", "Asignatura", 911, 20);
+		bib.agregarProfesor("p984", "Pedro", "Arrupe", "Jesuitas II", "Asignatura", 111, 15);
+		
+		bib.
+	}
+
+}
